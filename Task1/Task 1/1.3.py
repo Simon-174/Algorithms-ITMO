54e7b611c0735fbbc188c54acf9a144649f6a4e3
Simon-174 <71905270+Simon-174@users.noreply.github.com>
2020-09-26 00:24:51 +0300

    Add files via upload

---
+++ Task1/Task 1/1.3.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from timeit import default_timer
+from scipy.optimize import curve_fit
+
+
+# Implementing the algorithm in question
+def product(v):
+    answer = 1
+    for i in range(len(v)):
+        answer *= v[i]
+    return answer
+
+
+# Defining approximation function
+def line(x, a, b):
+    return np.multiply(a, x) + b
+
+
+# Counting average execution time for each n
+time_list = []
+for n in range(1, 2001):
+    v = np.random.rand(n)
+    exec_time = 0
+    for i in range(5):
+        start = default_timer()
+        product(v)
+        exec_time += (default_timer() - start)
+    time_list.append(exec_time / 5)
+
+# Creating the x-axis for plot making
+xdata = [n for n in range(1, 2001)]
+
+# Finding parameters of approximation function
+parameters = curve_fit(line, xdata, time_list)[0]
+
+# Plotting
+plt.plot(xdata, time_list, 'b', label='experimental data')
+plt.plot(xdata, line(xdata, *parameters), 'r', label='approximation')
+plt.title("the product of elements")
+plt.xlabel('n')
+plt.ylabel('Time, sec')
+plt.legend()
+plt.tight_layout()
+plt.show()
